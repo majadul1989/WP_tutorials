f86d3260a56cd5b76d88d75e11f74058d2cbf593
majaul1989 <majadul1989@gmail.com>
2017-08-09 01:26:09 +0600

    Update on 9/8/17

--- content.php
+++ content.php
@@ -1,10 +1,10 @@
 <div class="blog-post">
 	<h2 class="blog-post-title"><?php the_title(); ?></h2>
 	<p class="blog-post-meta">
-		<?php the_date(); ?> by
-		<a href="#">
-			<?php the_author(); ?>
-		</a>
+<small><?php the_time( 'F jS, Y' ); ?> by <?php the_author_posts_link(); ?></small>
+<small> <?php edit_post_link('Edit this entry.','',''); ?>
+</small>
+		
 	</p>
 
  <?php the_content(); ?>
--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php get_header(); ?>
 <?php if (have_posts()): while (have_posts()): the_post();
-	get_template_part( 'content', get_post_format() );
+	get_template_part( 'content', get_post_format() ); ?>
+<p class="postmetadata"><?php _e( 'Posted in' ); ?> <?php the_category( ', ' ); ?></p>
+<?php
 endwhile; //while end 
 ?>
 <div class="navigation">
-  <?php posts_nav_link(); //Pagenavications ?>
-</div>
+
+<?php echo paginate_links(); ?>
+
+
+  </div>
 <?php
 endif; // if end
 ?>
@@ -13,3 +18,7 @@ endif; // if end
 <?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
+
+
+
+ 
